5ecfcc54aaf7fe0b5b183448aaf3af13bcd663ec
陈孙旭 <chensunxu@163.com>
2018-12-18 16:19:55 +0800

    first commit

---
+++ common/js/common.js
@@ -0,0 +1,146 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = clientWidth / 7.5 + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
+var util = {}
+/**
+ * 返回随机名字
+ */
+util.getName = function () {
+    var familyNames = new Array(
+        "赵", "钱", "孙", "李", "周", "吴", "郑", "王", "冯", "陈",
+        "褚", "卫", "蒋", "沈", "韩", "杨", "朱", "秦", "尤", "许",
+        "何", "吕", "施", "张", "孔", "曹", "严", "华", "金", "魏",
+        "陶", "姜", "戚", "谢", "邹", "喻", "柏", "水", "窦", "章",
+        "云", "苏", "潘", "葛", "奚", "范", "彭", "郎", "鲁", "韦",
+        "昌", "马", "苗", "凤", "花", "方", "俞", "任", "袁", "柳",
+        "酆", "鲍", "史", "唐", "费", "廉", "岑", "薛", "雷", "贺",
+        "倪", "汤", "滕", "殷", "罗", "毕", "郝", "邬", "安", "常",
+        "乐", "于", "时", "傅", "皮", "卞", "齐", "康", "伍", "余",
+        "元", "卜", "顾", "孟", "平", "黄", "和", "穆", "萧", "尹"
+    );
+    var givenNames = new Array(
+        "子璇", "淼", "国栋", "夫子", "瑞堂", "甜", "敏", "尚", "国贤", "贺祥", "晨涛",
+        "昊轩", "易轩", "益辰", "益帆", "益冉", "瑾春", "瑾昆", "春齐", "杨", "文昊",
+        "东东", "雄霖", "浩晨", "熙涵", "溶溶", "冰枫", "欣欣", "宜豪", "欣慧", "建政",
+        "美欣", "淑慧", "文轩", "文杰", "欣源", "忠林", "榕润", "欣汝", "慧嘉", "新建",
+        "建林", "亦菲", "林", "冰洁", "佳欣", "涵涵", "禹辰", "淳美", "泽惠", "伟洋",
+        "涵越", "润丽", "翔", "淑华", "晶莹", "凌晶", "苒溪", "雨涵", "嘉怡", "佳毅",
+        "子辰", "佳琪", "紫轩", "瑞辰", "昕蕊", "萌", "明远", "欣宜", "泽远", "欣怡",
+        "佳怡", "佳惠", "晨茜", "晨璐", "运昊", "汝鑫", "淑君", "晶滢", "润莎", "榕汕",
+        "佳钰", "佳玉", "晓庆", "一鸣", "语晨", "添池", "添昊", "雨泽", "雅晗", "雅涵",
+        "清妍", "诗悦", "嘉乐", "晨涵", "天赫", "玥傲", "佳昊", "天昊", "萌萌", "若萌"
+    );
+    var i = parseInt(10 * Math.random()) * 10 + parseInt(10 * Math.random());
+    var familyName = familyNames[i];
+    var j = parseInt(10 * Math.random()) * 10 + parseInt(10 * Math.random());
+    var givenName = givenNames[i];
+    if (!familyName) familyName = familyNames[1]
+    if (!givenName) givenName = givenNames[1]
+    var name = familyName + givenName;
+    return name;
+}
+/**
+ * 返回浏览器内核
+ */
+util.versions = function () {
+    var u = navigator.userAgent,
+        app = navigator.appVersion;
+    return { //移动终端浏览器版本信息
+        trident: u.indexOf('Trident') > -1, //IE内核
+        presto: u.indexOf('Presto') > -1, //opera内核
+        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+        android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+        iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+        iPad: u.indexOf('iPad') > -1, //是否iPad
+        webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+        weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+        qq: u.match(/\sQQ/i) == " qq" //是否QQ
+    }
+}
+/**
+ * 复制
+ * @param {复制按钮id} idBtn 
+ * @param {要复制的文本}} copyText 
+ */
+util.copy = function (idBtn, copyText, type) {
+    // 重写alert弹窗，去掉title
+    window.alert = function (name) {
+        const iframe = document.createElement('IFRAME');
+        iframe.style.display = 'none';
+        iframe.setAttribute('src', 'data:text/plain,');
+        document.documentElement.appendChild(iframe);
+        window.frames[0].window.alert(name);
+        iframe.parentNode.removeChild(iframe);
+    };
+    if (type == 2) {
+        var clipboard = new Clipboard(idBtn);
+        var value = copyText;
+        $(idBtn).attr('data-clipboard-text', value);
+        clipboard.on('success', function (e) {
+            alert('复制成功');
+            clipboard.destroy();
+        });
+        clipboard.on('error', function (e) {
+            alert('复制失败');
+            clipboard.destroy();
+        });
+    } else {
+        $(idBtn).unbind('click').on('click', function () {
+            var clipboard = new Clipboard(idBtn);
+            var value = copyText;
+            $(idBtn).attr('data-clipboard-text', value);
+            clipboard.on('success', function (e) {
+                alert('复制成功');
+                clipboard.destroy();
+            });
+            clipboard.on('error', function (e) {
+                alert('复制失败');
+                clipboard.destroy();
+            });
+        })
+    }
+}
+/**
+ * 倒计时
+ * @param {时间戳} times 
+ * @param {回调} callback 
+ */
+// var timer = null;
+util.countDown = function (times, callback) {
+    //带天数的倒计时
+    var timer = setInterval(function () {
+        var day = 0,
+            hour = 0,
+            minute = 0,
+            second = 0; //时间默认值
+        day = Math.floor(times / (60 * 60 * 24));
+        hour = Math.floor(times / (60 * 60)) - (day * 24);
+        minute = Math.floor(times / 60) - (day * 24 * 60) - (hour * 60);
+        second = Math.floor(times) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
+
+        if (day <= 9) day = '0' + day;
+        if (hour <= 9) hour = '0' + hour;
+        if (minute <= 9) minute = '0' + minute;
+        if (second <= 9) second = '0' + second;
+        // console.log(day + "天:" + hour + "小时：" + minute + "分钟：" + second + "秒");
+        times--;
+        callback(day, hour, minute, second, times)
+        if (times < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+    return timer
+}
